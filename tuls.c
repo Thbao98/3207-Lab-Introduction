bfa6e1b19a72d3055a210ede5f69e365136d24a9
Thbao98 <118203614+Thbao98@users.noreply.github.com>
2023-02-01 12:39:17 -0500

    Copying the scandir in man page

--- tuls.c
+++ tuls.c
@@ -5,3 +5,20 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
+int main(int argc, char **argv){
+  struct dirent **namelist;
+  int n;
+  
+  n = scandir(".", &namelist, NULL, alphasort);
+  if(n == -1){
+    perror("scandir");
+    exit(EXIT_FAILURE);
+  }
+  while(n--){
+    printf("%s\n", namelist[n] -> d_name);
+    free(namelist[n]);
+  }
+  free(namelist);
+  exit(EXIT_SUCCESS);
+}
